1ccd065665e3194de191c35e1c03d68badd12a4f
David <DLee315@gmail.com>
2016-04-17 12:20:21 -0700

    added comments to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
-
+  get 'blog' => 'static_pages#blog'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
