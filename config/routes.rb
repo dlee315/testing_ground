9155fdc14e72bf14956affcc090eed90de4a4c2d
David <DLee315@gmail.com>
2016-04-19 00:10:37 -0700

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'about' => 'static_pages#about'
-  get 'blog' => 'static_pages#blog'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'blog' => 'static_pages#blog'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
