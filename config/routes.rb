0ee5ab8845a7e580b36a522902eac24347b90261
David <DLee315@gmail.com>
2016-04-22 10:31:46 -0700

    created session controller and login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'blog' => 'static_pages#blog'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
 
